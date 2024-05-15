885b4d8379463d808288c66629b3297bbd7772aa
paulAlexSerban <paul.alex.serban@gmail.com>
2024-05-15 23:38:34 +0300

    feat: centos start script and docker-compose for wordpress

---
+++ docker/centos/start.bash
@@ -0,0 +1,11 @@
+# start and run docker container with centos image
+# rub it detached and keep it running
+
+# start docker container
+docker run -d -it --name centos centos
+
+# get the os version
+docker exec -it centos cat /etc/os-release
+
+# run bash in the container
+docker exec -it centos bash
--- docker/traefik-ecs/openssl.bash
+++ docker/traefik-ecs/openssl.bash
@@ -1,4 +1,6 @@
-openssl req -x509 -out backend/proxy/openssl/localhost.crt -keyout backend/proxy/openssl/localhost.key \
+
+# Generate a self-signed certificate for localhost
+openssl req -x509 -out certs/openssl/localhost.crt -keyout certs/openssl/localhost.key \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=paulserban.eu' -extensions EXT -config <( \
    printf "[dn]\nCN=paulserban.eu\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:paulserban.eu\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
