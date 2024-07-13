37f102d7eb28724e10fd351b101b3dee09506d81
paulAlexSerban <paul.alex.serban@gmail.com>
2024-07-13 21:17:37 +0300

    docker: new verions react w vite

---
+++ docker/react-simple-app/vite-project/scripts.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+
+function build_docker_image_dev() {
+	docker build -t react-v2:${PACKAGE_VERSION}--dev -f Dockerfile.dev .
+	docker build -t react-v2:latest -f Dockerfile.dev .
+}
+
+function build_docker_image_prod() {
+	docker build -t react-v2:${PACKAGE_VERSION} -f Dockerfile.prod .
+	docker build -t react-v2:latest -f Dockerfile.prod .
+}
+
+function run_docker_container_dev() {
+	docker run -d -p 8080:5173 react-v2:${PACKAGE_VERSION}--dev
+}
+
+function run_docker_container_prod() {
+	docker run -d -p 8080:80 react-v2:${PACKAGE_VERSION}
+}
+
+function stop_docker_container() {
+	docker stop $(docker ps -q --filter ancestor=react-v2)
+}
+
+function remove_docker_container() {
+	docker rm $(docker ps -a -q --filter ancestor=react-v2)
+}
+
+function run_docker_container_latest() {
+	docker run -d -p 8080:80 react-v2:latest
+}
+
+function start-compose() {
+	docker compose down && docker compose up --build
+}
+
+$1
