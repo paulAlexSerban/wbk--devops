eb688c0fac73b561af02f279dc043696dd9c6a7c
paulAlexSerban <paul.alex.serban@gmail.com>
2024-07-16 23:16:35 +0300

    feat: nodejsfeedback app

---
+++ docker/docker/nodejs/nodejs_feedback-app/scripts.bash
@@ -0,0 +1,74 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+IMAGE_NAME=$(node -p -e "require('./package.json').name")
+CONTAINER_NAME=${IMAGE_NAME}
+
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+
+HOST_PORT=8080
+CONTAINER_PORT=5000
+
+IMAGE_REGISTRY_NAMESPACE=$(node -p -e "require('./package.json').imageRegistryNamespace")
+
+if [ -z "$1" ]; then
+    help
+    exit 1
+fi
+
+function help() {
+    echo "Available commands:"
+    echo "build - build the Docker image"
+    echo "run - run the Docker container"
+    echo "list_images - list all Docker images"
+    echo "list_containers - list all Docker containers"
+    echo "stop - stop the Docker container"
+    echo "remove - remove the Docker container"
+}
+
+function list_images() {
+    docker image ls | grep ${IMAGE_NAME}
+}
+
+function list_containers() {
+    docker ps | grep ${CONTAINER_NAME}
+}
+
+function build() {
+    docker image build --tag ${IMAGE_REGISTRY_NAMESPACE}/${IMAGE_NAME}:${PACKAGE_VERSION} \
+        --tag ${IMAGE_REGISTRY_NAMESPACE}/${IMAGE_NAME}:latest . \
+        --build-arg CONTAINER_PORT=${CONTAINER_PORT}
+    list_images
+}
+
+function stop() {
+    # stop the container
+    docker stop ${CONTAINER_NAME}
+}
+
+function remove() {
+    # remove the container
+    docker rm ${CONTAINER_NAME}
+    list_containers
+}
+
+function run() {
+    stop
+    remove
+    docker run --detach --name ${CONTAINER_NAME} \
+        -p ${HOST_PORT}:${CONTAINER_PORT} ${IMAGE_REGISTRY_NAMESPACE}/${IMAGE_NAME}:latest
+    list_containers
+    echo "Server listening to http://localhost:${HOST_PORT}" # Fixed message to use HOST_PORT
+}
+
+function push() {
+    docker push ${IMAGE_REGISTRY_NAMESPACE}/${IMAGE_NAME}:${PACKAGE_VERSION}
+    docker push ${IMAGE_REGISTRY_NAMESPACE}/${IMAGE_NAME}:latest
+}
+
+function logs() {
+    docker logs ${CONTAINER_NAME}
+}
+
+$1 && echo "Done" || echo "Failed"
