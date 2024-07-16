f37025b4618e92dddb62346e5aaada3f6d328117
paulAlexSerban <paul.alex.serban@gmail.com>
2024-07-16 20:40:16 +0300

    feat: adjust scripts.bash and Dockerfile

--- docker/docker/nginx_basic/scripts.bash
+++ docker/docker/nginx_basic/scripts.bash
@@ -2,30 +2,47 @@
 # makes sure the folder containing the script will be the root folder
 cd "$(dirname "$0")" || exit
 
-function build() {
-    docker image build -t customnginx:0.1.0 -t customnginx:latest .
+IMAGE_NAME="custom-nginx"
+VERSION=latest
+LOCALHOST_PORT=8080
+CONTAINR_NAME="custom-nginx"
 
-    echo "Custom Nginx image built"
-    echo "To check the image, use: "
-    echo "docker image ls | grep customnginx"
-    echo "bash scripts.bash list_images"
-}
-
-function run() {
-    docker run -d -p 8080:80 customnginx:latest
-
-    echo "Custom Nginx container running, open http://localhost:8080 in your browser"
-    echo "To check the running container, use:"
-    echo "docker ps | grep customnginx"
-    echo "bash scripts.bash list_containers"
+function help() {
+    echo "Available commands:"
+    echo "build - build the Docker image"
+    echo "run - run the Docker container"
+    echo "list_images - list all Docker images"
+    echo "list_containers - list all Docker containers"
 }
 
 function list_images() {
-    docker image ls | grep customnginx
+    docker image ls | grep ${IMAGE_NAME}
 }
 
 function list_containers() {
-    docker ps | grep customnginx
+    docker ps | grep ${CONTAINR_NAME}
+}
+
+function build() {
+    docker image build -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:latest . 
+    list_images
+}
+
+function run() {
+    docker run --name ${CONTAINR_NAME} -d -p ${LOCALHOST_PORT}:80 ${IMAGE_NAME}:latest --name ${CONTAINR_NAME}
+    list_containers
+    echo "Server listenting to http://localhost:${LOCALHOST_PORT}"
+}
+
+function stop() {
+    # stop the container
+    docker stop ${CONTAINR_NAME}
+}
+
+function remove() {
+    # remove the container
+    docker rm ${CONTAINR_NAME}
+    list_containers
 }
 
 $1
